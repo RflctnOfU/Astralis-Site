48bc43ac8727821a2197114cb3f473b6d0e0130a
Kris Marshall <rflctnofu@hotmail.com>
2025-05-30 00:59:48 -0400

    updated bios page

--- src/components/Bios.tsx
+++ src/components/Bios.tsx
@@ -107,75 +107,85 @@ function Bios() {
         </div>
       </div>
       <hr className="lg:hidden mx-4" />
+
       <div
         className="lg:grid lg:grid-cols-2 gap-4 bg-transparent flex flex-col-reverse"
-        ref={targetRef7}
+        ref={targetRef5}
       >
         <div className="md:p-4 p-2">
           <h2
-            className={`font-dream md:text-5xl text-4xl py-4 text-center`}
-            ref={targetRef7}
+            className={`md:text-5xl text-4xl py-4 text-center font-dream`}
+            ref={targetRef5}
             style={{
-              transform: isInView7 ? "none" : "translateX(-150%)",
-              opacity: isInView7 ? 1 : 0,
+              transform: isInView5 ? "none" : "translateX(-150%)",
+              opacity: isInView5 ? 1 : 0,
               transition: "all 1.75s cubic-bezier(0.17, 0.55, 0.55, 1) 0.09s",
             }}
           >
-            KRIS MARSHALL
+            LAURA USISKIN
           </h2>
           <h4
             className={`md:text-2xl text-xl font-light text-center font-mont`}
-            ref={targetRef7}
+            ref={targetRef5}
             style={{
-              transform: isInView7 ? "none" : "translateX(-150%)",
-              opacity: isInView7 ? 1 : 0,
+              transform: isInView5 ? "none" : "translateX(-150%)",
+              opacity: isInView5 ? 1 : 0,
               transition: "all 1.75s cubic-bezier(0.17, 0.55, 0.55, 1) 0.24s",
             }}
           >
-            TRUMPET
+            CELLO
           </h4>
           <p
             className={`font-mont py-2 lg:pl-4 text-center lg:text-left leading-8 font-extralight`}
-            ref={targetRef7}
+            ref={targetRef5}
             style={{
-              transform: isInView7 ? "none" : "translateX(-150%)",
-              opacity: isInView7 ? 1 : 0,
+              transform: isInView5 ? "none" : "translateX(-150%)",
+              opacity: isInView5 ? 1 : 0,
               transition: "all 1.75s cubic-bezier(0.17, 0.55, 0.55, 1) .39s",
             }}
           >
-            Kris Marshall is principal trumpet with the Southwest Florida
-            Symphony Orchestra, Venice Symphony, and has built an active career
-            performing with multiple orchestras across the state of Florida,
-            including the Jacksonville Symphony, Charlotte Symphony, Naples
-            Philharmonic, and regular performances with The Florida Orchestra.
-            Marshall has been a featured soloist with the Southwest Florida
-            Symphony Orchestra on many occasions and with the Fifth Avenue
-            Chamber Orchestra in Naples, Florida. As a founding member of the
-            Astralis Chamber Ensemble, he presents several chamber recital tours
-            each year and participates in commission projects to increase the
-            chamber repertoire for trumpet. He won first prize in the Florida
-            Orchestra Young Artist Competition and was later selected to attend
-            the Chautauqua Institution Summer Music Festival where he studied
-            with Chuck Berginc, principal trumpet with the Phoenix Symphony
-            Orchestra. Other significant instructors include Robert Smith and Ed
-            Cord. Marshall pursued degrees in trumpet performance at Florida
-            State University, Indiana University, and the University of South
-            Florida.
+            Laura Usiskin enjoys a versatile career as a cellist and educator.
+            Her playing has taken her to concert halls throughout the United
+            States and Europe, including the Kennedy Center, Palazzo Chigi
+            Saracini, Symphony Hall (Boston), Alice Tully Hall, Weill Hall,
+            Zankel Hall, Merkin Hall, Miller Theatre, Barge Music, Steinway
+            Hall, and Klavierhaus. While completing an Artist-In-Residency with
+            the Montgomery Symphony Orchestra, Usiskin founded and served as the
+            inaugural Executive Director of the Montgomery Music Project, an El
+            Sistema strings program in Montgomery, Alabama. The program, now
+            more than 10 years old, has given intensive string instruction to
+            hundreds of low-income children across three counties. Usiskin is a
+            member of the Bayberry String Quartet as well as the Atlanta-based
+            new music group ensemble vim. She has served on the faculty of the
+            University of Alabama at Birmingham, through which she founded the
+            chamber series Chamber Music @ AEIVA, as well as Birmingham-Southern
+            College. Usiskin graduated from Columbia University cum laude with a
+            Bachelor of Arts in Neuroscience and Behavior, The Juilliard School
+            with a Master of Music, and Yale University with a Doctor of Musical
+            Arts. For more information, visit{" "}
+            <Link
+              href={"https://www.laurausiskin.com"}
+              target="_blank"
+              className="text-[hsl(290,25%,73%)]"
+            >
+              www.laurausiskin.com
+            </Link>
+            .
           </p>
         </div>
 
         <div
-          className="fade-in-right m-8 bg-[url('/images/members/4.webp')] bg-no-repeat bg-cover rounded-xl shadow-lg shadow-[hsl(290,25%,23%)] lg:h-auto md:h-[450px] bg-center h-[350px]"
-          ref={targetRef7}
+          className="fade-in-right m-8 bg-[url('/images/members/10.webp')] bg-no-repeat bg-cover rounded-xl shadow-lg shadow-[hsl(290,25%,23%)] lg:h-auto md:h-[450px] bg-center h-[350px]"
+          ref={targetRef5}
           style={{
-            transform: isInView7 ? "none" : "translateX(150%)",
-            opacity: isInView7 ? 1 : 0,
+            transform: isInView5 ? "none" : "translateX(150%)",
+            opacity: isInView5 ? 1 : 0,
             transition: "all 1.75s cubic-bezier(0.17, 0.55, 0.55, 1) 0.09s",
           }}
         ></div>
       </div>
-
       <hr className="lg:hidden mx-4 self-center" />
+
       <div
         className="lg:grid lg:grid-cols-2 gap-4 bg-transparent flex flex-col"
         ref={targetRef2}
@@ -374,83 +384,77 @@ function Bios() {
         </div>
       </div>
       <hr className="lg:hidden mx-4 self-center" />
+
       <div
         className="lg:grid lg:grid-cols-2 gap-4 bg-transparent flex flex-col-reverse"
-        ref={targetRef5}
+        ref={targetRef7}
       >
         <div className="md:p-4 p-2">
           <h2
-            className={`md:text-5xl text-4xl py-4 text-center font-dream`}
-            ref={targetRef5}
+            className={`font-dream md:text-5xl text-4xl py-4 text-center`}
+            ref={targetRef7}
             style={{
-              transform: isInView5 ? "none" : "translateX(-150%)",
-              opacity: isInView5 ? 1 : 0,
+              transform: isInView7 ? "none" : "translateX(-150%)",
+              opacity: isInView7 ? 1 : 0,
               transition: "all 1.75s cubic-bezier(0.17, 0.55, 0.55, 1) 0.09s",
             }}
           >
-            LAURA USISKIN
+            KRIS MARSHALL
           </h2>
           <h4
             className={`md:text-2xl text-xl font-light text-center font-mont`}
-            ref={targetRef5}
+            ref={targetRef7}
             style={{
-              transform: isInView5 ? "none" : "translateX(-150%)",
-              opacity: isInView5 ? 1 : 0,
+              transform: isInView7 ? "none" : "translateX(-150%)",
+              opacity: isInView7 ? 1 : 0,
               transition: "all 1.75s cubic-bezier(0.17, 0.55, 0.55, 1) 0.24s",
             }}
           >
-            CELLO
+            TRUMPET
           </h4>
           <p
             className={`font-mont py-2 lg:pl-4 text-center lg:text-left leading-8 font-extralight`}
-            ref={targetRef5}
+            ref={targetRef7}
             style={{
-              transform: isInView5 ? "none" : "translateX(-150%)",
-              opacity: isInView5 ? 1 : 0,
+              transform: isInView7 ? "none" : "translateX(-150%)",
+              opacity: isInView7 ? 1 : 0,
               transition: "all 1.75s cubic-bezier(0.17, 0.55, 0.55, 1) .39s",
             }}
           >
-            Laura Usiskin enjoys a versatile career as a cellist and educator.
-            Her playing has taken her to concert halls throughout the United
-            States and Europe, including the Kennedy Center, Palazzo Chigi
-            Saracini, Symphony Hall (Boston), Alice Tully Hall, Weill Hall,
-            Zankel Hall, Merkin Hall, Miller Theatre, Barge Music, Steinway
-            Hall, and Klavierhaus. While completing an Artist-In-Residency with
-            the Montgomery Symphony Orchestra, Usiskin founded and served as the
-            inaugural Executive Director of the Montgomery Music Project, an El
-            Sistema strings program in Montgomery, Alabama. The program, now
-            more than 10 years old, has given intensive string instruction to
-            hundreds of low-income children across three counties. Usiskin is a
-            member of the Bayberry String Quartet as well as the Atlanta-based
-            new music group ensemble vim. She has served on the faculty of the
-            University of Alabama at Birmingham, through which she founded the
-            chamber series Chamber Music @ AEIVA, as well as Birmingham-Southern
-            College. Usiskin graduated from Columbia University cum laude with a
-            Bachelor of Arts in Neuroscience and Behavior, The Juilliard School
-            with a Master of Music, and Yale University with a Doctor of Musical
-            Arts. For more information, visit{" "}
-            <Link
-              href={"https://www.laurausiskin.com"}
-              target="_blank"
-              className="text-[hsl(290,25%,73%)]"
-            >
-              www.laurausiskin.com
-            </Link>
-            .
+            Kris Marshall is principal trumpet with the Southwest Florida
+            Symphony Orchestra, Venice Symphony, and has built an active career
+            performing with multiple orchestras across the state of Florida,
+            including the Jacksonville Symphony, Charlotte Symphony, Naples
+            Philharmonic, and regular performances with The Florida Orchestra.
+            Marshall has been a featured soloist with the Southwest Florida
+            Symphony Orchestra on many occasions and with the Fifth Avenue
+            Chamber Orchestra in Naples, Florida. As a founding member of the
+            Astralis Chamber Ensemble, he presents several chamber recital tours
+            each year and participates in commission projects to increase the
+            chamber repertoire for trumpet. He won first prize in the Florida
+            Orchestra Young Artist Competition and was later selected to attend
+            the Chautauqua Institution Summer Music Festival where he studied
+            with Chuck Berginc, principal trumpet with the Phoenix Symphony
+            Orchestra. Other significant instructors include Robert Smith and Ed
+            Cord. Marshall pursued degrees in trumpet performance at Florida
+            State University, Indiana University, and the University of South
+            Florida.
           </p>
         </div>
 
         <div
-          className="fade-in-right m-8 bg-[url('/images/members/10.webp')] bg-no-repeat bg-cover rounded-xl shadow-lg shadow-[hsl(290,25%,23%)] lg:h-auto md:h-[450px] bg-center h-[350px]"
-          ref={targetRef5}
+          className="fade-in-right m-8 bg-[url('/images/members/4.webp')] bg-no-repeat bg-cover rounded-xl shadow-lg shadow-[hsl(290,25%,23%)] lg:h-auto md:h-[450px] bg-center h-[350px]"
+          ref={targetRef7}
           style={{
-            transform: isInView5 ? "none" : "translateX(150%)",
-            opacity: isInView5 ? 1 : 0,
+            transform: isInView7 ? "none" : "translateX(150%)",
+            opacity: isInView7 ? 1 : 0,
             transition: "all 1.75s cubic-bezier(0.17, 0.55, 0.55, 1) 0.09s",
           }}
         ></div>
       </div>
+
       <hr className="lg:hidden mx-4 self-center" />
+
       {/* <div
         className="lg:grid lg:grid-cols-2 gap-4 bg-transparent flex flex-col"
         ref={targetRef6}
